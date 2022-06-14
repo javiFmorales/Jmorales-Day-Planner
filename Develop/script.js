0af2635e65d25443f456417870a3403a22bf4566
javiermorales <javiertoto-00@hotmail.com>
2022-06-13 21:46:13 -0700

    files

---
+++ Develop/script.js
@@ -0,0 +1,40 @@
+//displays current date and time using moment.js
+var currentDay = $("#currentDay")
+currentDay.text(moment().format('MMMM Do YYYY'));
+var currentHour = moment().hour();
+console.log(currentHour)
+
+
+$(".time-block").each(function () {
+//gets the value saved in local storage 
+  var blockHour = $(this).attr("id");
+  console.log(blockHour.split("-")[1])
+  console.log(blockHour.split("hour-")[1])
+  var key = localStorage.getItem(blockHour);
+  var value = $(this).children(".description");
+  value.val(key);
+  //color will change depending on time
+  if (blockHour.split("hour-")[1] < currentHour){
+    $(this).addClass("past")
+
+   }else if (blockHour.split("hour-")[1] == currentHour){
+     $(this).addClass("present")
+
+   }else {
+     $(this).addClass("future") 
+  }
+console.log(this)
+});
+//the activity will save in  localstorage 
+$(".saveBtn").on("click", function () {
+  console.log(this)
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, text)
+
+
+}); 
+
+
+//ad one for each timeblock
+// $("#hour-9 textarea" ).val(localStorage.getItem("hour-9"))
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Jmorales-day-planner
+This week's task was to create a day planner to complete this assignment i followed these steps:
+
+-Time blocks were added. 
+-styling was added using bootstap.
+-functionality was added using jquery javascript.
+-moment.js was used to set the correct time and date.
+-if statement were used to influence the color the time block is presented based on the current hour.
+
+to access the deployed webpage click here :
